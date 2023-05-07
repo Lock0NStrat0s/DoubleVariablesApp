05f68a8cb93c573d5d4587a5d86ce8e35577737a
Mohammad Jabir <mohammadj94@gmail.com>
2023-05-07 14:00:28 -0600

    review complete.

--- DoubleVariables/Program.cs
+++ DoubleVariables/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// doubles can have a decimal place - 1.23, 43
+
+double averageAge;
+
+averageAge = (43 + 22 + 62) / 3;    // using ints will return an int or whole number
+averageAge = (43.0 + 22 + 62) / 3;  // one double value will make the result a double
+
+Console.WriteLine(averageAge);
+
+// limited number of places before it cuts off
